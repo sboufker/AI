5db62dcc120f9cc37de4242ac81e1eabf96084be
sboufker <92600624+sboufker@users.noreply.github.com>
2021-10-16 13:22:12 +0000

    Add files via upload

---
+++ Assets/Pathfinding.cs
@@ -0,0 +1,248 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Pathfinding : MonoBehaviour {
+	public Transform seeker, target;
+	Grid grid;
+
+
+	void Awake() {
+		grid = GetComponent<Grid> ();
+	}
+
+
+	void Update() {
+		FindPath(seeker.position, target.position);
+		FindPathUCS(seeker.position, target.position);
+		FindPathBFS(seeker.position, target.position);
+		FindPathDFS(seeker.position, target.position);
+	}
+
+
+	void FindPath(Vector3 startPos, Vector3 targetPos) {
+		var WatchPath = new System.Diagnostics.Stopwatch();
+		WatchPath.Start();
+
+		Node startNode = grid.NodeFromWorldPoint(startPos);
+		Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+		List<Node> openSet = new List<Node>();
+		HashSet<Node> closedSet = new HashSet<Node>();
+		openSet.Add(startNode);
+
+		while (openSet.Count > 0) {
+			Node node = openSet[0];
+			for (int i = 1; i < openSet.Count; i ++) {
+				if (openSet[i].fCost < node.fCost || openSet[i].fCost == node.fCost) {
+					if (openSet[i].hCost < node.hCost)
+						node = openSet[i];
+				}
+			}
+
+			openSet.Remove(node);
+			closedSet.Add(node);
+
+			if (node == targetNode) {
+				RetracePath(startNode,targetNode);
+				return;
+			}
+
+			foreach (Node neighbour in grid.GetNeighbours(node)) {
+				if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+					continue;
+				}
+
+				int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
+				if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
+					neighbour.gCost = newCostToNeighbour;
+					neighbour.hCost = GetDistance(neighbour, targetNode);
+					neighbour.parent = node;
+
+					if (!openSet.Contains(neighbour))
+						openSet.Add(neighbour);
+				}
+			}
+		}
+		WatchPath.Stop();
+		Debug.Log($"Path Execution Time: {WatchPath.ElapsedMilliseconds} ms");
+	}
+
+
+	void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
+		var WatchUCS = new System.Diagnostics.Stopwatch();
+		WatchUCS.Start();
+
+		Node startNode = grid.NodeFromWorldPoint(startPos);
+		Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+		List<Node> openSet = new List<Node>();
+		HashSet<Node> closedSet = new HashSet<Node>();
+		openSet.Add(startNode);
+		
+		while (openSet.Count > 0) {
+			Node node = openSet[0];
+			for (int i = 1; i < openSet.Count; i ++) {
+				if (openSet[i].gCost < node.gCost || openSet[i].gCost == node.gCost) {
+						node = openSet[i];
+				}
+			}
+
+			openSet.Remove(node);
+			closedSet.Add(node);
+
+			if (node == targetNode) {
+				RetracePathUCS(startNode,targetNode);
+				return;
+			}
+
+			foreach (Node neighbour in grid.GetNeighbours(node)) {
+				if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+					continue;
+				}
+
+				neighbour.gCost = node.gCost;
+				neighbour.parent = node;
+
+				if (!openSet.Contains(neighbour))
+					openSet.Add(neighbour);
+			}
+		}
+		WatchUCS.Stop();
+		Debug.Log($"UCS Execution Time: {WatchUCS.ElapsedMilliseconds} ms");
+	}
+     
+ 
+	void FindPathBFS(Vector3 startPos, Vector3 targetPos) { 
+		var WatchBFS = new System.Diagnostics.Stopwatch();
+		WatchBFS.Start();
+
+		Node startNode = grid.NodeFromWorldPoint(startPos);
+		Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+      	Queue<Node> queueBFS = new Queue<Node>();
+	  	HashSet<Node> closedSet = new HashSet<Node>();
+
+		queueBFS.Enqueue(startNode);
+        
+		while(queueBFS.Count!=0) { 
+			Node currentNode= queueBFS.Dequeue();
+			if (currentNode == targetNode) {
+				RetracePathBFS(startNode,targetNode);
+				return;
+			}
+			closedSet.Add(currentNode);
+			foreach (Node neighbour in grid.GetNeighbours(currentNode))
+			{
+				if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+						continue;
+					}
+				if (neighbour.walkable || !queueBFS.Contains(neighbour)) {
+						closedSet.Add(neighbour);
+						neighbour.parent = currentNode; 
+						queueBFS.Enqueue(neighbour);
+					}
+			}
+		}
+		WatchBFS.Stop();
+		Debug.Log($"BFS Execution Time: {WatchBFS.ElapsedMilliseconds} ms");
+	}
+
+
+	void FindPathDFS(Vector3 startPos, Vector3 targetPos) { 
+		var WatchDFS = new System.Diagnostics.Stopwatch();
+		WatchDFS.Start();
+
+		Node startNode = grid.NodeFromWorldPoint(startPos);
+		Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+      	Stack<Node> StackDFS = new Stack<Node>();
+	  	HashSet<Node> closedSet = new HashSet<Node>();
+		StackDFS.Push(startNode);
+        
+		while(StackDFS.Count!=0) { 
+			Node currentNode= StackDFS.Pop();
+			if (currentNode == targetNode) {
+				RetracePathDFS(startNode,targetNode);
+				return;
+			}
+			closedSet.Add(currentNode);
+			foreach (Node neighbour in grid.GetNeighbours(currentNode))
+			{
+				if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+						continue;
+					}
+				if (neighbour.walkable || !StackDFS.Contains(neighbour)) {
+						closedSet.Add(neighbour);
+						neighbour.parent = currentNode; 
+						StackDFS.Push(neighbour);
+					}
+			}
+		}
+		WatchDFS.Stop();
+		Debug.Log($"DFS Execution Time: {WatchDFS.ElapsedMilliseconds} ms");
+	}
+
+
+	void RetracePath(Node startNode, Node endNode) {
+		List<Node> path = new List<Node>();
+		Node currentNode = endNode;
+
+		while (currentNode != startNode) {
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Reverse();
+		grid.path = path;
+	}
+
+
+	void RetracePathUCS(Node startNode, Node endNode) {
+		List<Node> path = new List<Node>();
+		Node currentNode = endNode;
+
+		while (currentNode != startNode) {
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Reverse();
+		grid.pathUCS = path;
+	}
+
+
+    void RetracePathBFS(Node startNode, Node endNode) {
+		List<Node> path = new List<Node>();
+		Node currentNode = endNode;
+
+		while (currentNode != startNode) {
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Reverse();
+		grid.pathBFS = path;
+	}
+
+
+    void RetracePathDFS(Node startNode, Node endNode) {
+		List<Node> path = new List<Node>();
+		Node currentNode = endNode;
+
+		while (currentNode != startNode) {
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Reverse();
+		grid.pathDFS = path;
+	}
+
+
+	int GetDistance(Node nodeA, Node nodeB) {
+		int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+		int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+		if (dstX > dstY)
+			return 14*dstY + 10* (dstX-dstY);
+		return 14*dstX + 10 * (dstY-dstX);
+	}
+}
